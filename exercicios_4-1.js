866d12834bb8cada7a5f02b5f9f9e2e405ac8892
Henrique <94632721+OsvaldoHenriqueSouza@users.noreply.github.com>
2022-03-07 19:41:29 -0300

    Adiciona exercicios_4.1

---
+++ exercicios_4-1.js
@@ -0,0 +1,109 @@
+// Primeira questão
+
+const num1 = 5;
+const num2 = 4;
+const comparacao = 7;
+
+
+// A seguir operação de adição
+
+let adicao = num1 + num2;
+
+// A seguir operação de subtração
+
+let subtracao = num1 - num2;
+
+// A seguir operação de multiplicação
+
+let multiplicacao = num1 * num2;
+
+// A seguir operação de divisão
+
+let divisao = num1 / num2;
+
+// Segunda questão
+
+if (num1 > num2) {
+    console.log("O num1 é maior que o num2.");
+}
+else {
+    console.log("O num2 é maior que o num1.");
+}
+
+// Terceira questão
+
+if (comparacao > num1 && comparacao > num2) {
+    console.log("O comparacao é maior que os demais.")
+}
+else if (num1 > comparacao && num1 > num2) {
+    console.log("O num1 é maior que os demais.")
+}
+else {
+    console.log("O num2 é maior que os demais.")
+}
+
+// Quarta questão
+
+let positiveNegativeZero = 9;
+
+if (positiveNegativeZero > 0) {
+    console.log("Positivo.")
+}
+else if (positiveNegativeZero === 0) {
+    console.log("Negativo.")
+}
+else {
+    console.log("Zero.")
+}
+
+// Quinta questão
+
+
+let angulo1 = 35;
+let angulo2 = 105;
+let angulo3 = 40;
+
+
+if (angulo1 + angulo2 + angulo3 == 180) {
+    console.log(true)
+}
+else {
+    console.log(false)
+}
+
+// Sexta questão
+
+let pecaXadrez = "bispo";
+
+
+
+// Sétima questão
+
+let porcentagem = 70;
+
+if (porcentagem < 50) {
+    console.log("F")
+}
+else if (porcentagem >= 50 && porcentagem < 60) {
+    console.log("E")
+}
+else if (porcentagem >= 60 && porcentagem < 70) {
+    console.log("D")
+}
+else if (porcentagem >= 70 && porcentagem < 80) {
+    console.log("C")
+}
+else  if (porcentagem >= 80 && porcentagem < 90) {
+    console.log("B")
+}
+else {
+    console.log("A")
+}
+
+// Oitava questão
+
+const num3 = 12;
+const num4 = 15;
+const num5 = 36;
+
+if ()
