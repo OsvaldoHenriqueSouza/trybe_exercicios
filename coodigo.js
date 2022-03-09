83b7bd58e119377bc5482d14c0cdf1d420184e31
Henrique <94632721+OsvaldoHenriqueSouza@users.noreply.github.com>
2022-03-09 19:36:40 -0300

    Adiciona a terceira questao

--- coodigo.js
+++ coodigo.js
@@ -18,14 +18,28 @@ console.log(palavraInvertida);
 
 // Terceira questao
 
+// Maior palavra
+
 let array = ['java', 'javascript', 'python', 'html', 'css'];
-let maiorPalavra = "";
+let maiorPalavra = array[0];
+let menorPalavra = array[0];
+
 for (let index3 = 0; index3 < array.length; index3++) {
-    if (index3[0] < array[index3]) {
-        maiorPalavra = array[index3];
+    if (array[index3].length < menorPalavra) {
+        menorPalavra = array[index3]
+    }
+}
+console.log(menorPalavra)
+
+for (let index4 = 0; index4 < array.length; index4++) {
+    if (array[index4].length > maiorPalavra.length) {
+        maiorPalavra = array[index4]
     }
 }
 console.log(maiorPalavra);
+
+
+
 
 // Quarta questao
 
