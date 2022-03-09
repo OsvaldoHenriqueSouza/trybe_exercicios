88fdb42f0d7b398f28e85caad9610df032aaa50f
Henrique <94632721+OsvaldoHenriqueSouza@users.noreply.github.com>
2022-03-09 19:14:24 -0300

    Modifica a segunda questao

--- coodigo.js
+++ coodigo.js
@@ -9,7 +9,23 @@ console.log(resultado);
 // Segunda questao
 
 let palavra = "trybe";
-let palavraInvertida = palavra.split("").reverse().join("");
+let palavraInvertida = "";
+for (let i = palavra.length-1; i >= 0; i-= 1) {
+    palavraInvertida += palavra[i]
+}
 
 console.log(palavraInvertida);
+
+// Terceira questao
+
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+let maiorPalavra = "";
+for (let index3 = 0; index3 < array.length; index3++) {
+    if (index3[0] < array[index3]) {
+        maiorPalavra = array[index3];
+    }
+}
+console.log(maiorPalavra);
+
+// Quarta questao
 
