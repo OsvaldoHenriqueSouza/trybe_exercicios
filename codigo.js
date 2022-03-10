1479c3e6b40dcbb64c007cfbee20947f079dd52c
Henrique <94632721+OsvaldoHenriqueSouza@users.noreply.github.com>
2022-03-10 18:36:50 -0300

    Adiciona a segunda questao

---
+++ codigo.js
@@ -0,0 +1,23 @@
+// Primeira questao
+
+
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  };
+
+  for (let key in info) {
+      console.log(`Bem vinda, ${info.personagem}`);
+  }
+
+//   Segunda questao
+
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  };
+
+  info["recorrente"] = "Sim";
+  console.log(info);
