a7fe852c856ef8bf0c5de840e982b5db412a2639
Henrique <94632721+OsvaldoHenriqueSouza@users.noreply.github.com>
2022-03-19 18:06:33 -0300

    Adiciona resolucao da questao 1

--- codigo.js
+++ codigo.js
@@ -15,9 +15,27 @@ function createDaysOfTheWeek() {
   
   // Escreva seu código abaixo.
 
+//   Exercício 1:
+const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
 
+const locUlDays = document.getElementById("days");
 
-//   Exercícios 2:
+function addNumDias() {
+    for (let i of dezDaysList) {
+        let liDay = document.createElement("li");
+        liDay.innerText = i;
+        liDay.classList = "day";
+        if (i == "24" || i == "25" || i == "31") {
+            liDay.classList.add("holiday")
+        }
+        if (i == "4" || i == "11" || i == "18" || i == "25") {
+            liDay.classList.add("friday");
+        }
+        locUlDays.appendChild(liDay); 
+    }
+}
+addNumDias();
+//   Exercício 2:
 
 let locBtnEntra = document.querySelector(".buttons-container");
 function criaBotao(nomeBtn) {
@@ -27,4 +45,6 @@ function criaBotao(nomeBtn) {
     locBtnEntra.appendChild(btnHoliday);
 }
 criaBotao("Feriados");
+
+// Exercício 3:
 
